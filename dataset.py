ce84f3da59e61e6697f52d78b406c5037957aedb
asarsembayev <dos_lp@mail.ru>
2023-03-06 13:12:16 +0600

    finished feature extractor

--- dataset.py
+++ dataset.py
@@ -2,15 +2,20 @@ import os
 
 import pandas as pd
 
+import torch
 from torch.utils.data import Dataset
+from torchaudio.transforms import MelSpectrogram, Resample
+from torchaudio import load as load_audio
 
 
 class AudioDataset(Dataset):
 	"""a custom pytorch Dataset for audio classifier"""
-	def __init__(self, src_dir, labels_path, transforms=None):
+	def __init__(self, src_dir, labels_path, transforms, target_sample_rate, train_duration):
 		self.src_dir = src_dir
 		self.labels = pd.read_csv(labels_path, sep='\t')
 		self.transforms = transforms
+		self.target_sample_rate = target_sample_rate
+		self.num_samples = int(train_duration * target_sample_rate)
 
 
 	def __len__(self):
@@ -21,4 +26,39 @@ class AudioDataset(Dataset):
 		audio_path = os.path.join(self.src_dir, self.labels.iloc[idx, 0])
 		label = self.labels.iloc[idx, 1]
 
-		return {'audio': audio_path, 'label': label}
+		signal, sr = load_audio(audio_path, normalize=True)
+		signal = self._resample_if_necessary(signal, sr)
+		signal = self._mix_down_if_necessary(signal)
+		signal = self._cut_if_necessary(signal)
+		signal = self._right_pad_if_necessary(signal)
+		signal = self.transforms(signal)
+
+		return signal, label
+
+
+	def _cut_if_necessary(self, signal):
+		if signal.shape[1] > self.num_samples:
+			signal = signal[:, :self.num_samples]
+		return signal
+
+
+	def _right_pad_if_necessary(self, signal):
+		length_signal = signal.shape[1]
+		if length_signal < self.num_samples:
+			num_missing_samples = self.num_samples - length_signal
+			last_dim_padding = (0, num_missing_samples)
+			signal = torch.nn.functional.pad(signal, last_dim_padding)
+		return signal
+
+
+	def _resample_if_necessary(self, signal, sr):
+		if sr != self.target_sample_rate:
+			resampler = Resample(sr, self.target_sample_rate)
+			signal = resampler(signal)
+		return signal
+
+
+	def _mix_down_if_necessary(self, signal):
+		if signal.shape[0] > 1:
+			signal = torch.mean(signal, dim=0, keepdim=True)
+		return signal
--- main.py
+++ main.py
@@ -2,17 +2,51 @@ import os
 import argparse
 from tqdm import tqdm
 
+import yaml
+
 import torch
 from torch.utils.data import Dataset, DataLoader
 from dataset import AudioDataset
+from torchaudio.transforms import MelSpectrogram, MFCC
+
+
+def read_configs(path):
+	with open(path) as handle:
+		configs = yaml.load(handle, Loader=yaml.FullLoader)
+	return configs
+
+
+def get_feature(feature_type, configs=None):
+	if configs:
+		features = {
+			'mel': MelSpectrogram(
+				sample_rate = configs['audio']['sr'], n_fft = configs['mel']['n_fft'], hop_length = configs['mel']['hop_length']
+				),
+			'mfcc': ''
+		}
+	else:
+		features = {
+			'mel': MelSpectrogram(),
+			'mfcc': MFCC()
+		}
+
+	return features[feature_type]
 
 
 def main(src_dir, labels_path, transforms):
-	train_dataset = AudioDataset(src_dir, labels_path, transforms=None)
-	test_dataset = AudioDataset(src_dir, labels_path, transforms=None)
+	audio_configs = read_configs(os.path.join('configs', 'train.yaml'))
+	configs = read_configs(os.path.join('configs', 'features.yaml'))
+	configs = {**audio_configs, **configs}
+	transform = get_feature(transforms, configs=configs)
+
+	TRAIN_DURATION = configs['train']['train_duration']
+
+	train_dataset = AudioDataset(src_dir, labels_path, transforms=transform, \
+		target_sample_rate=configs['audio']['sr'], train_duration=TRAIN_DURATION)
+	# test_dataset = AudioDataset(src_dir, labels_path, transforms=None)
 
 	train_dataloader = DataLoader(train_dataset, batch_size=5, shuffle=False)
-	test_dataloader = DataLoader(test_dataset, batch_size=1, shuffle=False)
+	# test_dataloader = DataLoader(test_dataset, batch_size=1, shuffle=False)
 
 	for sample in train_dataloader:
 		print(sample)
@@ -23,6 +57,6 @@ if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
 	parser.add_argument("src_dir", type=str, help="[input] path to directory with raw audio files")
 	parser.add_argument("labels_path", type=str, help="[input] path to csv w/ labels")
-	parser.add_argument("--transforms", type=str, help="[input] transforms")
+	parser.add_argument("--transforms", type=str, help="[input] transforms", default=None)
 	args = parser.parse_args()
 	main(args.src_dir, args.labels_path, args.transforms)
